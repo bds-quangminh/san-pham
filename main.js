4fb4b016be72266430fe4a0ee4f5e9b26b0986d1
vteles-manager <duongpm13vhomes@gmail.com>
2022-01-03 10:39:32 +0700

    test imgbb images

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+  var scale = document.getElementById("video").height / document.getElementById("video").width;
+  var width = window.screen.width;
+  var height = width * scale;
+  document.getElementById("video").width = width;
+  document.getElementById("video").height = height;
+};
+
 function oncall() {
   window.open('tel:0856130195', '_self');
 }
